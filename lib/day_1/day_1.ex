49b1c3ee843b436ea1c59f1aa46316d5bcf2f8e8
Jamie Maclean <jamie.maclean@gartner.com>
2022-12-06 16:26:33 +0100

    day 1

--- lib/day_1/day_1.ex
+++ lib/day_1/day_1.ex
@@ -19,17 +19,12 @@ defmodule Day1 do
   def max_held_by_elves(elves, number_of_elves) do
     Enum.map(elves, fn snacks ->
       String.split(snacks, "\n", trim: true)
-      |> convert_to_ints()
+      |> Enum.map(&String.to_integer/1)
       |> Enum.sum()
     end)
     |> take_max(number_of_elves)
     |> Enum.sum()
     |> IO.inspect(label: "Max calories held by #{number_of_elves} elves:")
-  end
-
-  defp convert_to_ints(array) do
-    Enum.reject(array, fn string -> string == "" end)
-    |> Enum.map(&String.to_integer/1)
   end
 
   defp take_max(array, number_of_elves, answer \\ [])
@@ -39,7 +34,7 @@ defmodule Day1 do
 
   defp take_max(array, number_of_elves, answer) do
     max = Enum.max(array)
-    array = Enum.reject(array, fn a -> a == max end)
-    take_max(array, number_of_elves, [max | answer])
+    remaining_elves = Enum.reject(array, fn a -> a == max end)
+    take_max(remaining_elves, number_of_elves, [max | answer])
   end
 end
