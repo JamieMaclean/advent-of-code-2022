645377276dad0eb289e2f9c26cb3f659668d616e
Jamie Maclean <jamie.maclean@gartner.com>
2022-12-07 23:24:15 +0100

    day 7

---
+++ test/day_7/day_7_test.exs
@@ -0,0 +1,23 @@
+defmodule Day7Test do
+  use ExUnit.Case
+
+  alias Day7
+  alias Day7.{Directory, Filesystem, Action}
+
+  test "completes challenge successfully" do
+    assert Day7.run("test/day_7/input.txt") == {95437, 24933642}
+  end
+
+  describe "update_file_system/3" do
+    test "add a directory to the base_dir" do
+      filesystem =
+        %Filesystem{}
+        |> Filesystem.update_file_system(%Action{
+          action: :add,
+          data: %Directory{name: "level_1 dir"}
+        })
+
+      assert [%Directory{}] = filesystem.filesystem.contents
+    end
+  end
+end
