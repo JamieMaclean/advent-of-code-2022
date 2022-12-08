953e7b2934e18c4d5ff6dfef75a7107710031330
Jamie Maclean <jamie.maclean@gartner.com>
2022-12-08 11:46:50 +0100

    format

--- test/day_8/day_8_test.exs
+++ test/day_8/day_8_test.exs
@@ -5,6 +5,7 @@ defmodule Day8Test do
   alias Day8.Tree
 
   test "completes challenge successfully" do
-    assert Day8.run("test/day_8/input.txt") == {21, %Tree{size: 5, viewing_distance: 8, visible: true}}
+    assert Day8.run("test/day_8/input.txt") ==
+             {21, %Tree{size: 5, viewing_distance: 8, visible: true}}
   end
 end
