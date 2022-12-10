330ad41f9e7f5f330a8c5e17ec9500e0d67b794e
Jamie Maclean <jamie.maclean@gartner.com>
2022-12-10 21:36:58 +0100

    clean a little

--- lib/day_10/day_10.ex
+++ lib/day_10/day_10.ex
@@ -1,18 +1,22 @@
 defmodule Day10 do
-  alias Day10.Circuit
-
   def run(file) do
-    instructions = get_instructions(file)
+    state = %{
+      instructions: get_instructions(file),
+      cycle: 0,
+      value: 1,
+      blocked: {0, 0},
+      samples: [],
+      sprites: [],
+      sample_at: [20, 60, 100, 140, 180, 220]
+    }
 
-    {:ok, circuit} = Circuit.start_link([instructions, [20, 60, 100, 140, 180, 220]])
-
-    {part_1, part_2} =
-      give_instructions(circuit)
-      |> Circuit.get_samples()
+    %{samples: part_1, sprites: part_2} = tick(state)
 
     Enum.chunk_every(part_2, 40)
+    |> Enum.reverse()
     |> Enum.each(fn a ->
-      IO.puts(a)
+      Enum.reverse(a)
+      |> IO.puts()
     end)
 
     {Enum.sum(part_1), part_2}
@@ -31,11 +35,62 @@ defmodule Day10 do
     end
   end
 
-  defp give_instructions(circuit) do
-    case Circuit.tic(circuit) do
-      :ok -> give_instructions(circuit)
-      :blocked -> give_instructions(circuit)
-      :finished -> circuit
+  defp tick(
+         %{
+           value: value,
+           instructions: instructions,
+           blocked: {blocked_for, then_add}
+         } = state
+       ) do
+    case {blocked_for, instructions} do
+      {1, _} ->
+        new_state = %{take_sample(state) | blocked: {0, 0}, value: value + then_add}
+
+        tick(new_state)
+
+      {0, []} ->
+        state
+
+      {0, _} ->
+        new_state =
+          take_sample(state)
+          |> do_instruction(instructions)
+
+        tick(new_state)
+
+      _ ->
+        new_state = %{take_sample(state) | blocked: {blocked_for - 1, then_add}}
+
+        tick(new_state)
+    end
+  end
+
+  defp do_instruction(state, [{"noop", _} | rest]) do
+    %{state | instructions: rest}
+  end
+
+  defp do_instruction(state, [{"addx", number} | rest]) do
+    %{state | blocked: {1, number}, instructions: rest}
+  end
+
+  defp take_sample(%{sample_at: sample_at, value: value, cycle: cycle, samples: samples} = state) do
+    if (cycle + 1) in sample_at do
+      %{state | samples: [value * cycle | samples]}
+    else
+      state
+    end
+    |> tock()
+  end
+
+  defp tock(%{cycle: cycle, value: value, sprites: sprites} = state) do
+    %{state | cycle: cycle + 1, sprites: [sprite(value, cycle) | sprites]}
+  end
+
+  def sprite(value, cycle) do
+    if rem(cycle, 40) in (value - 1)..(value + 1) do
+      "#"
+    else
+      "."
     end
   end
 end
